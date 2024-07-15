6024d6f83dbb55b05392482a7c9711e2da3e85b1
sarabeccerica <sarabeccerica@gmail.com>
2024-07-15 11:04:10 +0200

    modifiche logica

--- offChain/model/model.py
+++ offChain/model/model.py
@@ -1,3 +1,5 @@
+import json
+
 from web3 import Web3
 
 
@@ -5,5 +7,22 @@ class Model:
 
     def __init__(self, provider_url,contract_name):
         self.web3 = Web3(Web3.HTTPProvider(provider_url))
-        self.contract_name = contract_name
-        self.contract = self.web3.eth.contract(address=self.contract_address, abi=abi)
+        try:
+            abi= self.get_contract_abi(contract_name)
+            address=self.get_contract_address(contract_name)
+            self.contract = self.w3.eth.contract(address=address, abi=abi)
+        except Exception as e:
+            self.deploy_contract(contract_name)
+
+    def get_contract_abi(self, contract_name):
+        with open(f'onChain/abi/{contract_name}.txt') as f:
+            contract_abi = json.load(f)
+        return contract_abi
+
+    def get_contract_address(self, contract_name):
+        with open(f'onChain/address/{contract_name}.txt') as f:
+            contract_address = f.read().strip()
+        return contract_address
+
+    def deploy_contract(self, contract_name):
+        pass
